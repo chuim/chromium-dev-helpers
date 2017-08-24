bacc31f53b14dd11f3e6d432249603a856e68ceb
Carlos Knippschild <carlosk@chromium.org>
2017-08-24 09:07:49 -0700

    Add doc comments to tests-from-target.py

--- tests-from-target.py
+++ tests-from-target.py
@@ -1,5 +1,31 @@
 #!/usr/bin/python
 
+# Have you ever been annoyed by having to manually harvest test class names so
+# that you can run a comprehensive set of tests from a Chromium subcomponent?
+# Yes, well, me too! And here's the solution for it.
+#
+# This script generates a Google Test formatted filter string using all C++ test
+# classes it can find given an output dir and one or more GN targets. For
+# instance this call:
+#
+# $ tests-from-target.py out_linux/Debug components/offline_pages/core/background:unit_tests
+#
+# Generates this output as of today:
+#
+# AddRequestTaskTest.*:ChangeRequestsStateTaskTest.*:CleanupTaskTest.*:
+# GetRequestsTaskTest.*:InitializeStoreTaskTest.*:MarkAttemptAbortedTaskTest.*:
+# MarkAttemptCompletedTaskTest.*:MarkAttemptStartedTaskTest.*:
+# PickRequestTaskTest.*:ReconcileTaskTest.*:RemoveRequestsTaskTest.*:
+# RequestCoordinatorEventLoggerTest.*:RequestCoordinatorTest.*:
+# RequestQueueStoreSQLTest.*:RequestQueueTest.*:SavePageRequestTest.*
+#
+# The most common usage case will be to use your shell's command substitution so
+# that the script populates the filter argument of your test execution call.
+# For Bash it would be something like this (if you are cool and use cr! ;):
+#
+# cr run components_unittests --gtest_filter=$(tests-from-target.py <OUT_DIR> <GN_TEST_TARGET>)
+#
+
 import sys
 import re
 import os.path
